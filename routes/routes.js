0a1e50467e5b8f47e68edec35177635998c1b17c
dongphuongman <dong.phuong.man@gmail.com>
2019-10-18 22:26:22 +0700

    Initial commit

---
+++ routes/routes.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = function (app) {
+	const categories = require('../controllers/categories')
+	const notes = require('../controllers/notes')
+
+	app.get('/notes', notes.getNotes)
+	app.get('/notes/:id', notes.getNotesById)
+	app.get('/notes/category/:catid', notes.getNotesByCategories)
+	app.post('/notes/', notes.postNotes)
+	app.patch('/notes/:id', notes.patchNotes)
+	app.delete('/notes/:id', notes.deleteNotes)
+
+	app.get('/categories', categories.getCategories)
+	app.get('/categories/:id', categories.getCategoriesById)
+	app.post('/categories/', categories.postCategory)
+	app.patch('/categories/:id', categories.patchCategory)
+	app.delete('/categories/:id', categories.deleteCategory)
+
+	
+}
